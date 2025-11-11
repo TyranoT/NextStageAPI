2146bc27afd42ee5f5f82e17a140b54238b9d9f1
Italo Git <sulamita.italo12@gmail.com>
2025-11-10 21:18:32 -0300

    feito

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,8 @@ import cors from "@fastify/cors";
 import { ControllerUsers } from "../controller/usuarios";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
-const app = Fastify({ logger: true });
+// Cria a instância do Fastify apenas uma vez
+const app = Fastify({ logger: false });
 
 // Configuração de CORS
 app.register(cors, {
@@ -15,27 +16,13 @@ app.register(cors, {
 
 // Rotas
 app.get("/", async (request, reply) => {
-  reply.send({ hello: "world" });
+  return { hello: "world" };
 });
 
-// Usuários
 app.post("/cadastrar", ControllerUsers.CriarUsuario);
 app.post("/entrar", ControllerUsers.LoginUsuario);
 
-// Start
-const start = async () => {
-  try {
-    await app.listen({ port: 3000, host: "localhost" });
-    console.log("Server running on http://localhost:3000");
-  } catch (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-};
-
-start();
-
-export default async (req: VercelRequest, res: VercelResponse) => {
-    await app.ready();
-    app.server.emit("request", req, res);
-};
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  await app.ready();
+  app.server.emit("request", req, res);
+}
